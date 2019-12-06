f3cb38adbf70cca2a64c4e0e31c13c4193d707de
vikramkarthikeyan <vkarthik@buffalo.edu>
2019-12-05 20:32:32 -0500

    Added server file

--- ir-app/app.js
+++ ir-app/app.js
@@ -27,6 +27,13 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
+  next();
+});
+
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
--- ir-app/routes/index.js
+++ ir-app/routes/index.js
@@ -1,10 +1,43 @@
 var express = require('express');
+const request = require('request');
+const appHttp = require('http');
+
+const server = 'http://localhost:8983';
+const core = 'tweets2';
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index1', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
+
+router.post('/getTweets', (req, res) => {
+
+  req.on('data', function (body) {
+    
+    result = JSON.parse(body);
+    console.log("test " + result.query);
+
+    var full_query = server+'/solr/'+core+'/select?q=tweet_text%3A%20' + encodeURIComponent(result.query) + '&defType=edismax&qf=tweet_text&wt=json&indent=true&rows=20&start=0';
+    
+    results = checkData(full_query, function (data) {
+      res.send(data);
+    });
+    
+  });
+
+});
+
+function checkData(full_query, callback) {
+  console.log("full_query " + full_query);
+
+  request(full_query, { json: true }, (err, response, body) => {
+    if (err) { return console.log(err); }
+    // res.send(body);
+    callback(body)
+  });
+
+}
 
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Express' });
